55e392223c15d7103e194bb986599fe92878018b
João Luís <103074041+jaulis@users.noreply.github.com>
2022-08-23 09:17:05 -0300

    Add files via upload

---
+++ Sistemas de informação/2ºperiodo/linguagem de progamação 1/lista 2/ex2-lista2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void geraMat(int N,int M){
+	
+	int mat[N][M],flagMaior=0,i,j;
+	
+	srand(time(NULL));
+	
+	for(i=0;i<N;i++){
+		for(j=0;j<M;j++){
+			mat[i][j]=rand()%100;
+			if(mat[i][j]>flagMaior){
+				flagMaior = mat[i][j];
+			}
+			printf("%d|",mat[i][j]);
+		}
+		printf("\n");
+	}	
+
+	printf("O maior numero eh: %d",flagMaior);
+	
+}
+
+int main(){
+	
+	int N,M;
+	
+	printf("Digite o numero de linhas : ");
+	scanf("%d",&N);
+	printf("Digite o numero de colunas: ");
+	scanf("%d",&M);
+	
+	geraMat(N,M);
+	
+	return 0;
+}
