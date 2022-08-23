9988de5362b768854f81e9e90a3bdd28c3208202
João Luís <103074041+jaulis@users.noreply.github.com>
2022-08-23 17:28:07 -0300

    Add files via upload

---
+++ Sistemas de informação/2ºperiodo/linguagem de progamação 1/lista 2/ex6-lista2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void geraMat(int N,int M){
+	
+	int mat[N][M],i,j,t=M;
+	
+	srand(time(NULL));
+	
+	for(i=0;i<N;i++){
+		for(j=0;j<M;j++){
+			mat[i][j]=rand()%10;
+			printf("%d|",mat[i][j]);
+		}
+		printf("\n");
+	}	
+	
+	printf("\n");
+	
+	for(i=0;i<N;i++){
+	t--;
+		for(j=0;j<M;j++){
+			if(j==t){
+				printf("%d|",mat[i][j]);
+			}
+		}
+	}
+	
+}
+
+int main(){
+	
+	int N,M;
+	
+	
+	printf("Digite o numero de linhas : ");
+	scanf("%d",&N);
+	printf("Digite o numero de colunas: ");
+	scanf("%d",&M);
+	
+	geraMat(N,M);
+	
+	return 0;
+}
