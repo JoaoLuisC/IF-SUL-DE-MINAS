ad3e6e8003f6a0c517cf9c923918308d245585b6
João Luís <103074041+jaulis@users.noreply.github.com>
2022-09-28 22:07:17 -0300

    Update ex13-lista2.c

--- Sistemas de informação/2ºperiodo/linguagem de progamação 1/lista 2/ex13-lista2.c
+++ Sistemas de informação/2ºperiodo/linguagem de progamação 1/lista 2/ex13-lista2.c
@@ -1,102 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-void scanMat(int m[][100],int lin,int cols){
-	
-	int i,j;
-	
-	for(i=0;i<lin;i++)
-		for(j=0;j<cols;j++){
-			printf("matriz[%d][%d]: ",i,j);
-			scanf("%d",&m[i][j]);
-			system("cls");
-		}
-			
-	system("cls");
-}
-
-void printMat(int m[][100],int lin,int cols){
-	
-	int i,j;
-	
-	for(i=0;i<lin;i++){
-		for(j=0;j<cols;j++){
-			printf("%d|",m[i][j]);
-		}
-		printf("\n");
-	}		
-	
-}
-
-void multiMat(int m1[2][3],int m2[3][2],int lin1,int lin2,int cols1,int cols2){
-	
-	int i,j,k,l=0; //  k = lin2 ; l = cols1;
-	
-	int m3[lin1][cols2];
-	
-	for(i=0;i<lin1;i++)
-		for(j=0;j<cols2;j++)
-			m3[i][j]=0;
-
-	
-	for(i=0;i<lin1;i++){
-			
-		for(j=0;j<cols1;j++){
-			l=0;
-			for(k=0;k<lin2;k++){
-				m3[i][l]+=(m1[i][j]*m2[k][l]);
-				printf("m3[%d][%d]+=m1[%d][%d]*m2[%d][%d]: %d ",i,j,i,l,k,j,m3[i][l]);
-				system("pause");
-				l++;
-			}
-			printf("\n");
-		}
-	}
-	
-	printf("\n");
-	printMat(m3,lin1,cols2);
-	
-
-} 
 
 int main(){
+
+	int M[2][3], N[3][2], mult[2][2], i, j, aux, k = 0, mi = 0, mj, l;
+
+
+	 printf("Informe Valores para Matriz 01: \n");
+	 for(i = 0; i < 2; i++)
+	 	for(j = 0; j < 3; j++)
+	 		scanf("%d", &M[i][j]);
 	
-	int m1[2][3]={1,0,2,-1,3,1},m2[3][2]={3,1,2,1,1,0},newM[100][100],lin1,cols1,lin2,cols2;
 	
-	printf("Digite o numero de linhas da primeira matriz: ");
-	scanf("%d",&lin1);
-	printf("Digite o numero de colunas da primeira matriz: ");
-	scanf("%d",&cols1);
-	
-	printf("Digite o numero de linhas da segunda matriz: ");
-	scanf("%d",&lin2);
-	printf("Digite o numero de colunas da segunda matriz: ");
-	scanf("%d",&cols2);
-	
+	 printf("\nInforme Valores para Matriz 02: \n");
+	 for(i = 0; i < 3; i++)
+	 	for(j = 0; j < 2; j++)
+	 		scanf("%d", &N[i][j]);
+
 	system("cls");
 	
-	//scanMat(m1,lin1,cols1);
-	//scanMat(m2,lin2,cols2);
-		
-	//printMat(m1,lin1,cols1);
-	//printf("\n");
-	//printMat(m2,lin2,cols2);
-	int i,j;
-	for(i=0;i<lin1;i++){
-		for(j=0;j<cols1;j++){
-			printf("%d|",m1[i][j]);
+	while(k != 2){
+		l = 0;
+		mj = 0;
+		while(l != 2){
+			aux = 0;
+			j = 0;
+			i = 0;
+			while(j != 3){
+				aux += (M[k][j] * N[i][l]); 
+				i++;
+				j++;	
+			}
+			mult[mi][mj] = aux;	
+			mj++;
+			l++;
 		}
-		printf("\n");
+		mi++;	
+		k++;
 	}
-	
-	for(i=0;i<lin2;i++){
-		for(j=0;j<cols2;j++){
-			printf("%d|",m2[i][j]);
-		}
+	 
+	for(i = 0; i < 2; i++){
 		printf("\n");
+		for(j = 0; j < 2; j++)
+				printf("%d | ", mult[i][j]);
 	}
-		
-	multiMat(m1,m2,lin1,lin2,cols1,cols2);	
+
 	return 0;
 }
