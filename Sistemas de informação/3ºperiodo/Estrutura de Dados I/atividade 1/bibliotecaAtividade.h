1a445d878e3ae209d7597fe5bd08be1e5534da7a
João Luís Cardoso <103074041+JoaoLuisC@users.noreply.github.com>
2023-02-28 08:46:05 -0300

    Add files via upload

---
+++ Sistemas de informação/3ºperiodo/Estrutura de Dados I/atividade 1/bibliotecaAtividade.h
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define MAX 10
+
+typedef  struct{
+	char descricao[50];
+	char data[20];
+}tdado;
+
+typedef struct{
+	tdado pilha[MAX];
+	int topo;
+}tPilha;
+//--------------------
+void inicializa(tPilha *p){
+	p->topo = -1;
+}
+//----------------------
+int push(tPilha *p, tdado dado){
+	if(p->topo == MAX-1) // cheio
+		return 0;
+	p->topo++;
+	p->pilha[p->topo] = dado;	
+	return 1;
+}
+//------------------------
+tdado pop(tPilha *p){
+	tdado removido = p->pilha[p->topo];
+	p->topo--;
+	return removido;
+}
+//---------------------------
+tdado peek(tPilha p){
+	return p.pilha[p.topo];
+}
+//------------------------
+int isEmpty(tPilha p){
+	if (p.topo == -1)
+	  return 1; // vazia
+	else
+	  return 0;  
+}
+//------------------------
+void mostraPilha(tPilha p){
+	while(p.topo> -1){
+		printf("%s - %s |",p.pilha[p.topo].descricao,p.pilha[p.topo].data );
+		p.topo--;
+	}// fim while
+	printf("\n");
+}
