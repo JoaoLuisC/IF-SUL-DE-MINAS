fd4401409fc37713ad9e38738f7dce034e62654d
João Luís Cardoso <103074041+JoaoLuisC@users.noreply.github.com>
2023-03-15 22:26:50 -0300

    Add files via upload

---
+++ Sistemas de informação/3ºperiodo/Estrutura de Dados I/JavaPallets/src/javapalletscontrol/JavaPallet.java
@@ -0,0 +1,10 @@
+package javapalletscontrol;
+
+public class JavaPallet {
+
+    public static void main(String[] args) {
+        FormSistema f = new FormSistema();
+        f.setVisible(true);
+    }
+    
+}
