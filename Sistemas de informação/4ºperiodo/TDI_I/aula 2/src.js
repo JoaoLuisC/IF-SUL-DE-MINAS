afb859b8630abf5229f0f060a2db6e21c77b59f4
Joao Luís Cardoso <joao.lc12@hotmail.com>
2023-08-16 07:25:25 -0300

    aulas de tdi I att

---
+++ Sistemas de informação/4ºperiodo/TDI_I/aula 2/src.js
@@ -0,0 +1,8 @@
+function mudarCores(){          
+    title1.style.color = 'red';
+    div1.style.background = 'gray';
+    div1.innerHTML = "<p> oi gente</p>";
+    
+}
+
+title1.addEventListener('click', mudarCores);
