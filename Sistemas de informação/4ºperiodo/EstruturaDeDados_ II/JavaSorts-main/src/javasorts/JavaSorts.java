1569a822c492af7f74dbf41589867d25a5613a30
Joao Luís Cardoso <joao.lc12@hotmail.com>
2023-08-16 08:28:46 -0300

    Slection Sort

--- Sistemas de informação/4ºperiodo/EstruturaDeDados_ II/JavaSorts-main/src/javasorts/JavaSorts.java
+++ Sistemas de informação/4ºperiodo/EstruturaDeDados_ II/JavaSorts-main/src/javasorts/JavaSorts.java
@@ -1,22 +1,65 @@
 package javasorts;
 import java.util.Random;
 import java.util.Scanner;
+
 public class JavaSorts {
+    
+    static int menuEntrada(){
+     
+        Scanner scanner = new Scanner(System.in);
+        
+        int op;
+        
+        System.out.println("1- Gerar dados");
+        System.out.println("2- Entrar com dados");
+        op = scanner.nextInt();
+                
+        return op; 
+    }
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
-        int tamanho;
+        int tamanho,op;
+        
+        
         System.out.println("Tamanho do vetor:");
         tamanho = scanner.nextInt();
         int[] vetor = new int[tamanho];
-        System.out.println("Dados vetor:");
-        for(int i=0;i<vetor.length;i++)
-            vetor[i] = scanner.nextInt();
+        
+        
+        op = menuEntrada();
+        
+        switch(op){
+            
+            case 1:
+                for(int i=0;i<vetor.length;i++){
+                        vetor[i] = rand.nextInt(100);
+                }
+            break;
+            
+            case 2:
+                System.out.println("Dados vetor:");
+                    for(int i=0;i<vetor.length;i++){
+                        vetor[i] = scanner.nextInt();
+                    }
+            break;
+            
+            
+        }
+                
+        
         System.out.println("Vetor original:");
         printArray(vetor);
-        BubbleSort.bSortComentado(vetor);
+        
+//      BubbleSort.bSortComentado(vetor);
+//      SelectionSort.sSort(vetor);
+        SelectionSort.sSortComentado(vetor);
+                
         System.out.println("Vetor ordenado");
         printArray(vetor);
+        
     }
     
     public static void printArray(int array[]){
---
+++ Sistemas de informação/4ºperiodo/EstruturaDeDados_ II/JavaSorts-main/src/javasorts/SelectionSort.java
@@ -0,0 +1,64 @@
+package javasorts;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    
+    
+    private static int indexMinArray(int array [],int posAtual){   //menor valor do Array
+       
+        int K = posAtual; //variavel de menor posição 
+            
+        //loop para percorrer o array e achar o menor valor
+        for(int i = posAtual; i<array.length; i++){
+            
+            if(array[i] < array[K])//se o array na posição atual for menor q a posição atual ele altera a 
+                 K = i;            //variavel K para o novo menor
+                         
+            
+        }
+      
+        return K;
+    } 
+    
+    
+    public static int sSort(int array[]){
+        
+        for(int i = 0; i < array.length-1;i++){
+            int k = indexMinArray(array, i);
+            
+            //trocar valores de i com k
+            
+            int temp = array[i];
+            array[i] = array[k];
+            array[k] = temp;
+            
+        }
+        
+        
+        return 0;
+    }
+    
+     public static int sSortComentado(int array[]){
+        
+        Scanner scanner = new Scanner(System.in);
+         
+        for(int i = 0; i < array.length-1;i++){
+            int k = indexMinArray(array, i);
+            
+            System.out.println("Atual: "+array[i] + " Trocando com o menor valor: " + array[k]);
+            scanner.nextLine();//pausa
+            //trocar valores de i com k
+            
+            int temp = array[i];
+            array[i] = array[k];
+            array[k] = temp;
+            
+        }
+        
+        
+        return 0;
+    }
+    
+
+}
