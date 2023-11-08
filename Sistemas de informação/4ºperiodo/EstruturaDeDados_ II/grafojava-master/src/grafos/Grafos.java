2a6fe191a888502154c35d2f3e206ac5faf26967
Joao Luís Cardoso <joao.lc12@hotmail.com>
2023-11-08 08:22:17 -0300

    grafos - EST 2

---
+++ Sistemas de informação/4ºperiodo/EstruturaDeDados_ II/grafojava-master/src/grafos/Grafos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grafos;
+
+
+public class Grafos {
+
+    public static void main(String[] args) {
+        Graph grafo = new Graph();
+        grafo.adicionarVertice(1);
+        grafo.adicionarVertice(2);
+        grafo.adicionarVertice(3);
+        grafo.adicionarVertice(4);
+
+        grafo.adicionarAresta(1, 2, 3);
+        grafo.adicionarAresta(1, 3, 5);
+        grafo.adicionarAresta(2, 4, 2);
+        grafo.adicionarAresta(3, 4, 1);
+
+        grafo.imprimirGrafo();
+        
+    
+    }
+    
+}
