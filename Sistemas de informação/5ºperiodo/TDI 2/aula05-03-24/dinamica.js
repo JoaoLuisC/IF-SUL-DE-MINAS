39b30c30125c7bbe0fdaafb1b3b08f4ec8d61afa
João Luis Carodoso <joao.lc12@hotmail.com>
2024-03-08 07:10:29 -0300

    aula 08-03-24 js

---
+++ Sistemas de informação/5ºperiodo/TDI 2/aula05-03-24/dinamica.js
@@ -0,0 +1,21 @@
+//Dinamica
+
+ let x ;
+
+ console.log(x);
+ console.log(typeof(x));
+ console.log("----------");
+
+ let din = 3.33333;
+
+ console.log(din);
+ console.log(din.toFixed());
+ console.log(typeof din);
+ console.log("----------");
+ 
+ din = "emerson";
+
+ console.log(din);
+ console.log(typeof din);
+ console.log(din.toUpperCase());
+
